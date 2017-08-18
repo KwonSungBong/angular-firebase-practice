10a8dbde0bca5b27f8b44a386c3758404b958c9b
Kwon Sungbong <sb.kwon@anypointmedia.com>
2017-08-18 11:19:56 +0900

    test

--- hybrid/practice/src/app/app.module.ts
+++ hybrid/practice/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -10,6 +13,15 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAT-u_1kO7J9Ll5-y0AiZUa0OFh5q7NblA",
+  authDomain: "test-fcd82.firebaseapp.com",
+  databaseURL: "https://test-fcd82.firebaseio.com",
+  projectId: "test-fcd82",
+  storageBucket: "test-fcd82.appspot.com",
+  messagingSenderId: "589313533361"
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +33,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- hybrid/practice/src/pages/home/home.ts
+++ hybrid/practice/src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  user: Observable<firebase.User>;
 
+  constructor(public navCtrl: NavController, private afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  signIn() {
+    const email="rnjstjdqhd39@naver.com";
+    const password="rnjstjdqhd39@";
+
+    this.afAuth.auth.signInWithEmailAndPassword(email, password).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
